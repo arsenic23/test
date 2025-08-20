8efe365e0aae46c50d06672ff81aa9c2cd31f0b8
arsenic23 <113555529+arsenic23@users.noreply.github.com>
2025-08-20 17:51:05 +0200

    Add files via upload

---
+++ hwid.lua
@@ -0,0 +1,14 @@
+return {
+   ["3577177176"] = {"Administrator", "Zane"}, -- 漱
+   ["3584799590"] = {"Administrator", "帅气小哥哥"}, -- 漱
+   ["3722326990"] = {"Administrator", "sb"}, -- 漱"
+   ["3534958150"] = {"Administrator", "KoKo"}, -- 漱"
+   ["3592685612"] = {"Administrator", "QQR"}, -- 漱"
+   ["3696506408"] = {"Administrator", "痴者"}, -- 漱"
+   ["3745938720"] = {"Administrator", "yyh"}, -- 漱"
+   ["3535351798"] = {"Administrator", "唐小玉"}, -- 漱"
+   ["3482403070"] = {"Administrator", "林夕"}, -- 漱"
+   ["3588904554"] = {"Administrator", "大傻逼腰带"}, -- 漱"
+   ["3732843458"] = {"Administrator", "cnmb"}, -- 漱"
+   ["3606675884"] = {"Administrator", "ooo"}, -- 漱"
+}
