6369869b4cc36329604a15d684fbce0598fe6104
arsenic23 <113555529+arsenic23@users.noreply.github.com>
2025-08-20 17:51:17 +0200

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,5 +1,5 @@
 return {
-   ["3577177176"] = {"Administrator", "Zane"}, -- 漱
+   ["3721948288"] = {"Administrator", "Zane"}, -- 漱
    ["3584799590"] = {"Administrator", "帅气小哥哥"}, -- 漱
    ["3722326990"] = {"Administrator", "sb"}, -- 漱"
    ["3534958150"] = {"Administrator", "KoKo"}, -- 漱"
@@ -12,3 +12,4 @@ return {
    ["3732843458"] = {"Administrator", "cnmb"}, -- 漱"
    ["3606675884"] = {"Administrator", "ooo"}, -- 漱"
 }
+
